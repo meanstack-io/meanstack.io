d9c91e9a654f5b364bf6f3482be04c5588923677
Rafael Pegorari <rafapegorari@gmail.com>
2016-09-10 11:22:43 -0300

    Methods route AngularJS.

--- resources/assets/javascripts/angular/providers/route.js
+++ resources/assets/javascripts/angular/providers/route.js
@@ -1,8 +1,37 @@
 angular.module("App")
-    .config(['config', 'routeProvider', function (config, routeProvider) {
+    .config(['routeProvider', '$stateProvider', '$urlRouterProvider', 'config',
+        function (routeProvider, $stateProvider, $urlRouterProvider, config) {
 
-        /**
-         * Config route module
-         */
-        routeProvider.config(config.route);
-    }]);
+            /**
+             * Config route module MEANStack
+             */
+            routeProvider.config(config.route);
+
+            /**
+             * Error routes.
+             */
+            $stateProvider
+                .state('404', {
+                    templateNamespace: "error/404",
+                    data: {title: 'Page not found'}
+                })
+
+                .state('403', {
+                    templateNamespace: "error/403",
+                    data: {title: 'Not authorized'}
+                })
+
+                .state('500', {
+                    templateNamespace: "error/500",
+                    data: {title: 'Internal server error'}
+                });
+
+            /**
+             * Error 404.
+             * If requested route not registered.
+             */
+            $urlRouterProvider.otherwise(function ($injector, $location) {
+                $injector.get('$state').go('404');
+                return $location.path();
+            });
+        }]);
--- resources/assets/javascripts/angular/run.js
+++ resources/assets/javascripts/angular/run.js
@@ -1,19 +1,6 @@
 angular.module("App")
-    .run(["$rootScope", "$state", function ($rootScope, $state) {
-        // State to be accessed from view
-        $rootScope.$state = $state;
+    .run(["route", function (route) {
+        // bootstrap templateNamespace and redirect to error pages.
+        route.bootstrap();
 
-        // If error handler go to page error angular.
-        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-
-            event.preventDefault();
-
-            if (error.status === 404) {
-                return $state.go('404');
-            } else if (error.status === 403) {
-                return $state.go('403');
-            }
-
-            return $state.go('500');
-        });
     }]);
